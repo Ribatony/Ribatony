6e0758225754f5a029c48475fd461607eb59cff7
Ribatony <152007408+Ribatony@users.noreply.github.com>
2024-10-31 15:58:09 +0300

    Add files via upload

---
+++ user_ imput.py
@@ -0,0 +1,7 @@
+# Asking for user inputs and storing them in variables
+name = input("What is your name? ")
+age = input("What is your age? ")
+location = input("Where do you live? ")
+
+# Printing a personalized message using the inputs
+print(f"Hello {name}, you are {age} years old and live in {location}.")
